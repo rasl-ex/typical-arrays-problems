25f9e0fa890930d5cce30969217f05141c6f0c28
Ruslan <r.salikhov96@gmail.com>
2020-02-24 06:23:23 +0500

    feat: add implementation

--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
-
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+    return array && array.length > 0 ? Math.min.apply(null, array) : 0;
+}
+exports.max = function max(array) {
+    return array && array.length > 0 ? Math.max.apply(null, array) : 0;
 }
 
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+    return array && array.length > 0 ? array.reduce((acc, cur) => acc += cur, 0) / array.length : 0;
 }
